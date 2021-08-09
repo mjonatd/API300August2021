1eb52e260fdba350fed5f6b1cc76f79343369810
Jonathan Morgan <morganprogramming@gmail.com>
2021-08-09 16:33:37 -0400

    End of day 1

---
+++ src/AppointmentsSolution/AppointmentsApi/Services/DoctorAppointmentFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentsApi.Services
+{
+    public class DoctorAppointmentFinder : ILookupDoctorAppointmentTimes
+    {
+
+        public Task<DateTime> GetNextAvailableAppoinmentFor(string doctor)
+        {
+            var random = new Random();
+            return Task.FromResult(DateTime.Now.AddDays(random.Next(3, 5)));
+        }
+    }
+}
---
+++ src/AppointmentsSolution/AppointmentsApi/Services/ILookupDoctorAppointmentTimes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AppointmentsApi.Services
+{
+    public interface ILookupDoctorAppointmentTimes
+    {
+        Task<DateTime> GetNextAvailableAppoinmentFor(string doctor);
+    }
+}
